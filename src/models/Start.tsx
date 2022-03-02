f1bb9d9ff0459b85166c1ad1cc395b43a3280802
Reina Ogo <62832934+ogorei@users.noreply.github.com>
2022-03-02 22:23:29 +0900

    initial commit

---
+++ src/models/Start.tsx
@@ -0,0 +1,23 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Tab1.css';
+
+const Start: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Start;
